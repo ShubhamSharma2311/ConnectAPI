b5a05c52a523b12e86a107b307830de5b3a5ddea
ShubhamSharma2311 <sharma.2022ug2068@iiitranchi.ac.in>
2025-03-07 21:43:13 +0530

    commited after solving error in admin page backned

--- backend/src/controllers/adminController.ts
+++ backend/src/controllers/adminController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import Admin from "../models/Admin";
 import jwt from "jsonwebtoken";
+import { z } from "zod";
+import dotenv from "dotenv";
 dotenv.config();
 
 const adminSchema = z.object({
